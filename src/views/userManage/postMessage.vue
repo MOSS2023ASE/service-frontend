96435e53d9919b1ba4a65c1ba0d881e84e790732
wangxiaoge2001 <2353414299@qq.com>
2023-05-31 13:01:30 +0800

    [feat](api):完成前端beta阶段管理端工作量统计接口对接

--- src/views/userManage/postMessage.vue
+++ src/views/userManage/postMessage.vue
@@ -5,10 +5,13 @@
       <v-col :cols="8">
         <v-text-field label="通知标题" v-model="title" style="width: 300px;"></v-text-field>
         <v-textarea label="通知内容" v-model="content"></v-textarea>
-        <v-text-field label="统计图类型（0：折线图，1：饼图）" v-model="type" style="width: 300px;"></v-text-field>
-        <v-text-field label="统计指标（0：辅导师回答问题数，1：辅导师复审问题数，2：issue访问次数）" v-model="indicator" style="width: 300px;"></v-text-field>
+<!--        <v-text-field label="统计图类型（0：折线图，1：饼图）" v-model="type" style="width: 300px;"></v-text-field>-->
+<!--        <v-text-field label="统计指标（0：辅导师回答问题数，1：辅导师复审问题数，2：issue访问次数）" v-model="indicator" style="width: 300px;"></v-text-field>-->
         <v-text-field label="开始日期（yyyy-mm-dd）" v-model="begin_date" style="width: 300px;"></v-text-field>
         <v-text-field label="结束日期（yyyy-mm-dd）" v-model="end_date" style="width: 300px;"></v-text-field>
+        <v-text-field label="bonus_per_issue" v-model="bonus_per_issue" style="width: 300px;"></v-text-field>
+        <v-text-field label="bonus_per_counsel" v-model="bonus_per_counsel" style="width: 300px;"></v-text-field>
+        <v-text-field label="bonus_per_review" v-model="bonus_per_review" style="width: 300px;"></v-text-field>
       </v-col>
       <v-spacer></v-spacer>
     </v-row>
@@ -18,8 +21,14 @@
             <v-btn color="blue" class="white--text" width="80px" @click="broadcast">发送</v-btn>
         </v-col>
       <v-col class="d-flex justify-center">
-        <v-btn color="blue" class="white--text" width="80px" @click="getStatistics">调用统计图接口</v-btn>
+        <v-btn color="blue" class="white--text" width="80px" @click="getStudentBonus">学生志愿</v-btn>
       </v-col>
+      <v-col class="d-flex justify-center">
+        <v-btn color="blue" class="white--text" width="80px" @click="getTutorBonus">辅导师志愿</v-btn>
+      </v-col>
+<!--      <v-col class="d-flex justify-center">-->
+<!--        <v-btn color="blue" class="white&#45;&#45;text" width="80px" @click="getStatistics">调用统计图接口</v-btn>-->
+<!--      </v-col>-->
         <v-spacer></v-spacer>
     </v-row>
   </v-container>
@@ -28,7 +37,7 @@
 <script>
 import { getToken } from '@/utils/auth';
 import { admin_broadcast } from '@/api/notify';
-import {get_statistics} from '@/api/statistics'
+import {get_statistics, get_tutor_bonus, get_student_bonus} from '@/api/statistics'
 import { Message } from 'element-ui';
 export default {
   data() {
@@ -37,8 +46,13 @@ export default {
       content: "",
       type: '',
       indicator: '',
-      begin_date: '',
-      end_date: '',
+      begin_date: '2023-05-01',
+      end_date: '2023-05-31',
+      bonus_per_issue: 0.5,
+      bonus_per_counsel: 1.0,
+      bonus_per_review: 0.5,
+      max_bonus: 100,
+      min_bonus: 0,
     }
   },
   methods: {
@@ -55,6 +69,38 @@ export default {
         console.log(error)
         Message({
           message: '统计图获取失败',
+          type: 'error'
+        });
+      });
+    },
+    getStudentBonus() {
+      get_student_bonus(getToken(), parseFloat(this.bonus_per_issue), this.begin_date,
+        this.end_date, this.min_bonus, this.max_bonus).then(response => {
+        console.log(response)
+        Message({
+          message: '获取成功',
+          type: 'success'
+        });
+      }).catch(error => {
+        console.log(error)
+        Message({
+          message: '获取失败',
+          type: 'error'
+        });
+      });
+    },
+    getTutorBonus() {
+      get_tutor_bonus(getToken(), parseFloat(this.bonus_per_counsel), parseFloat(this.bonus_per_review), this.begin_date,
+        this.end_date, this.min_bonus, this.max_bonus).then(response => {
+        console.log(response)
+        Message({
+          message: '获取成功',
+          type: 'success'
+        });
+      }).catch(error => {
+        console.log(error)
+        Message({
+          message: '获取失败',
           type: 'error'
         });
       });
